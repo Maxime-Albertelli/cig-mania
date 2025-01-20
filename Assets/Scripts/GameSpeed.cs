311fe928a794ab1a7909e412861df5156a68b465
Maxime-Albertelli <maxime.albertelli@gmail.com>
2025-01-20 16:06:08 +0100

    Amélioration visuel pause

--- Assets/Scripts/GameSpeed.cs
+++ Assets/Scripts/GameSpeed.cs
@@ -1,6 +1,7 @@
 using TMPro;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Change the speed of the game onclick
@@ -10,7 +11,10 @@ using UnityEngine;
 public class GameSpeed : MonoBehaviour
 {
     int speedValue = 1;
-    [SerializeField] private TMP_Text buttonText; //don't forget to put the button's which change game speed label
+    [SerializeField] private RawImage imageMenuPause; //don't forget to put the button's which change game speed label
+    [SerializeField] private Texture vitesseDeux;
+    [SerializeField] private Texture vitesseUn;
+    [SerializeField] private Texture pause;
 
     public void gameSpeed()
     {
@@ -19,17 +23,17 @@ public class GameSpeed : MonoBehaviour
             case 1:
                 Time.timeScale = 2; //change game speed
                 speedValue = 2; //change the variable used by the switch which indicate the game speed
-                buttonText.text = "x2"; //change the button's label text to indicate game speed to the player
+                imageMenuPause.texture = vitesseDeux; //change the button's label text to indicate game speed to the player
                 break;
             case 2:
                 Time.timeScale = 0;
                 speedValue = 0;
-                buttonText.text = "Pause";
+                imageMenuPause.texture = pause;
                 break;
             case 0:
                 Time.timeScale = 1;
                 speedValue = 1;
-                buttonText.text = "x1";
+                imageMenuPause.texture = vitesseUn;
                 break;
         }
     }
