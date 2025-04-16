7e8e16d3b8baf0d1497820340beb5ae473d158b5
Leïla P. <leila.karukera@gmail.com>
2025-04-16 02:32:10 +0200

    Add TutorialManager script

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// This class manages the player's choice to play in tutorial mode or not
+public class TutorialManager : MonoBehaviour
+{
+    // Using a static class member means its value will be shared across all instances of this class
+    public static TutorialManager Instance;
+    // The TutorialStatus variable stores the player's choice regarding tutorial mode
+    public bool TutorialStatus;
+
+    // Awake is called as soon as the object is created
+    private void Awake()
+    {
+        // Ensure that only a single instance of the TutorialManager class can exist
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        // Ensure that the GameObject is preserved when loading a new scene
+        DontDestroyOnLoad(gameObject);
+    }
+}
